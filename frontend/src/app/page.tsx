a81e04bd923cd6f09728006142618c6fee6192a7
VijayenraNayak <vijayendranayak19@gmail.com>
2024-11-17 15:51:49 +0530

    border

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -31,17 +31,17 @@ export default function Home() {
       {/* First Section */}
       <section className="relative max-w-7xl mt-20 flex gap-20 mx-auto justify-between items-center h-[calc(100vh-160px)]">
         <div className="flex flex-col gap-6">
-          <div className="flex bg-white bg-opacity-75 px-6 items-center gap-8 shadow-xl hover:shadow-2xl justify-center rounded-2xl py-4 max-w-fit">
+          <div className="flex bg-white bg-opacity-75 border px-6 items-center gap-8 shadow-xl hover:shadow-2xl justify-center rounded-2xl py-4 max-w-fit">
             <Image src="/logo1.png" alt="logo" height={70} width={70} />
             <div className="text-6xl font-serif font-semibold">VTO</div>
           </div>
-          <div className="flex bg-white bg-opacity-75 px-3 py-2 gap-3 shadow-xl hover:shadow-2xl rounded-2xl flex-col">
+          <div className="flex bg-white bg-opacity-75 px-3 py-2 gap-3 shadow-xl border hover:shadow-2xl rounded-2xl flex-col">
             <h2 className="text-lg font-semibold text-purple-500">Interactive Virtual Try-On</h2>
             <p className="text-gray-600">
               Experience a seamless virtual try-on system for effortless online shopping.
             </p>
           </div>
-          <div className="flex bg-white bg-opacity-75 px-3 py-2 gap-3 shadow-xl hover:shadow-2xl rounded-2xl flex-col max-w-fit">
+          <div className="flex bg-white bg-opacity-75 px-3 py-2 border gap-3 shadow-xl hover:shadow-2xl rounded-2xl flex-col max-w-fit">
             <h2 className="text-lg font-semibold text-purple-500">3D Clothing Visualization</h2>
             <p className="text-gray-600">
               Visualize how clothes fit and look on a realistic 3D model for precise design previews.
