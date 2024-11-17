25047554f2be7149c948f4b9561620528b58ccf2
VijayenraNayak <vijayendranayak19@gmail.com>
2024-11-17 19:06:28 +0530

    proxy and env

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // Match all API requests
+        destination: 'http://127.0.0.1:5000/:path*', // Forward them to the backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
