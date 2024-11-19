1bef619b7670e14eb8d511a54fa6a2848057f819
chandan <chandanb200317@gmail.com>
2024-11-19 11:22:42 +0530

    for

--- frontend/src/app/unauthorized/page.tsx
+++ frontend/src/app/unauthorized/page.tsx
@@ -3,10 +3,11 @@
 import { useRouter } from "next/navigation";
 
 export default function ForbiddenPage() {
-  const router=useRouter()
+  const router = useRouter();
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="text-center p-8 bg-white rounded-lg shadow-md">
+      <div 
+        className="text-center p-8 bg-white rounded-lg shadow-md relative z-10">
         <h1 className="text-6xl font-bold text-red-500 mb-4">403</h1>
         <h2 className="text-2xl font-semibold text-gray-700 mb-4">
           Access Forbidden
@@ -15,10 +16,12 @@ export default function ForbiddenPage() {
           Sorry, you don't have permission to access this page.
         </p>
         <button 
-        className="px-4 rounded-full py-2 bg-purple-500 hover:bg-purple-600 " onClick={()=>router.push("/")}>
-          return home
+          className="px-4 py-2 rounded-full bg-purple-500 hover:bg-purple-600 text-white"
+          onClick={() => router.push("/")}>
+          Return Home
         </button>
       </div>
+      <div className="absolute inset-0 bg-gray-900 opacity-50 z-0"></div>
     </div>
   );
 }
